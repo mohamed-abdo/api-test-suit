e9bc9ad32c006214f9cd8d9d1d85f749b90b07f4
mohamed.abdou <Mohamed.Abdou@emaratech.ae>
2019-07-03 11:15:20 +0400

    refactoring runner

--- postman-runner.js
+++ postman-runner.js
@@ -4,72 +4,70 @@ const request = require('request');
 const fs = require('fs');
 const fs_extra = require('fs-extra');
 
-const resourceDir = '.\\resources\\api-response';
-const archiveDir = '.\\archive';
-
 const params = {
     traceService: process.argv[2],
     collection: process.argv[3],
-    environment: process.argv[4]
+    environment: process.argv[4],
+    resourceDir: '.\\resources\\api-response',
+    archiveDir: '.\\archive',
+    executionId: +new Date()
 }
 
-newman.run({
-        collection: require(params.collection),
-        environment: require(params.environment),
-        reporters: 'cli'
-    }, function (err, summary) {
-        if (err) {
-            throw err;
-        }
-        console.log('collection run complete! ', _.get(summary, 'run.timings'));
-    })
-    .on('start', function (err, args) {
-        archivePrevExecutionSync();
-        createExecutionDir();
-        console.log('on start ...');
-    })
-    .on('request', function (err, args) {
-        const requestBody = {
-            testCase: args.item.name,
-            status: args.response.code,
-            response_time: args.response.responseTime,
-            headers: _.fromPairs(args.request.headers.members.map(v => [v.key, v.value])),
-            request: args.request.body.raw,
-            response: _.get(args, 'response.body.raw'),
-        }
-        writeRequest(requestBody.testCase, JSON.stringify(requestBody));
-        postRequest(params.traceService, 'tracer', requestBody);
-    })
-    .on('error', function (err, summary) {
-        if (err || summary.error) {
-            console.error('collection run encountered an error.');
-        } else {
-            postRequest(params.traceService, 'summary', summary);
-            console.log('collection run completed.');
-        }
-    })
-    .on('done', function (err, summary) {
-        if (err || summary.error) {
-            console.error('collection run encountered an error.');
-        } else {
-            postRequest(params.traceService, 'summary', summary);
-            console.log('collection run completed.');
-        }
-    });
+const postmanRunnner = function (params) {
+    try {
+        newman.run({
+                collection: require(params.collection),
+                environment: require(params.environment),
+                reporters: 'cli'
+            }, function (err, summary) {
+                if (err) {
+                    throw err;
+                }
+                console.log('collection run complete! ', _.get(summary, 'run.timings'));
+            })
+            .on('start', function (err, args) {
+                archivePrevExecutionSync(params.resourceDir, params.archiveDir, params.executionId);
+                createExecutionDir(params.resourceDir);
+                console.log('on start ...');
+            })
+            .on('request', function (err, args) {
+                const requestBody = {
+                    testCase: args.item.name,
+                    status: args.response.code,
+                    response_time: args.response.responseTime,
+                    headers: _.fromPairs(args.request.headers.members.map(v => [v.key, v.value])),
+                    request: args.request.body.raw,
+                    response: _.get(args, 'response.body.raw'),
+                }
+                writeRequest(params.resourceDir, requestBody.testCase, JSON.stringify(requestBody));
+                postRequest(params.resourceDir, params.traceService, 'tracer', requestBody);
+            })
+            .on('done', function (err, summary) {
+                if (err || summary.error) {
+                    console.error('collection run encountered an error.');
+                } else {
+                    postRequest(params.traceService, 'summary', summary);
+                    console.log('collection run completed.');
+                }
+            });
+    } catch (err) {
+        console.error(`error while executing postman collection, ${err}`)
+    }
+};
 
 const postRequest = function (traceService, endpoint, requestBody) {
-    const traceServiceEndpoint = `${traceService}/${endpoint}/${requestBody.id}`;
+    const traceServiceEndpoint = `${traceService}/${endpoint}/${executionId}`;
     console.log(`on request:${requestBody.testCase}, calling: ${traceServiceEndpoint} service`);
     request.post(traceServiceEndpoint, {
         json: {
             data: requestBody
         }
-    }, (error, res, body) => {
+    }, (error, res) => {
         if (error) {
-            console.error(`error on while sending tarce to ${traceService}, test case: ${requestBody.testCase}, error: ${error}`)
+            console.error(`error on while sending tarce to ${traceService}, error: ${error}`)
             return
         }
-        console.log(`statusCode: ${res.statusCode}, for test case: ${requestBody.testCase}`)
+        console.log(`statusCode: ${res.statusCode}, of service: ${traceService}`)
     });
 };
 
@@ -79,17 +77,17 @@ const defaultCallbacl = function (err) {
 };
 
 
-const writeRequest = function (testCase, json) {
+const writeRequest = function (resourceDir, testCase, json) {
     fs_extra.writeFile(`${resourceDir}\\${testCase}.json`, json, defaultCallbacl);
 };
 
-const createExecutionDir = function () {
+const createExecutionDir = function (resourceDir) {
     if (!fs.existsSync(resourceDir))
         fs.mkdirSync(resourceDir, {
             recursive: true
         }, defaultCallbacl);
 }
-const archivePrevExecutionSync = function () {
+const archivePrevExecutionSync = function (resourceDir, archiveDir, executionId) {
     fs.readdir(resourceDir, {
         withFileTypes: true
     }, (err, files) => {
@@ -97,9 +95,12 @@ const archivePrevExecutionSync = function () {
             console.error(err);
         else
             files.forEach(file => {
-                const folderName = +new Date();
-                console.info(`moving directory: ${file.name} to ${archiveDir}\\${folderName}\\${file.name}`);
-                fs_extra.moveSync(`${resourceDir}\\${file.name}`, `${archiveDir}\\${folderName}\\${file.name}`);
+
+                console.info(`moving directory: ${file.name} to ${archiveDir}\\${executionId}\\${file.name}`);
+                fs_extra.moveSync(`${resourceDir}\\${file.name}`, `${archiveDir}\\${executionId}\\${file.name}`);
             });
     });
 };
+
+// executing postman runner
+postmanRunnner(params);
